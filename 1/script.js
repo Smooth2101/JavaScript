a24cdd5893707f3d1cb94949e06b0b2c78a3a5b4
Zhenya <zh.grebyonkin@yandex.ru>
2021-01-31 16:00:17 +0500

    31.01

--- 1/script.js
+++ 1/script.js
@@ -1,17 +1,24 @@
 let money = prompt("Ваш бюджет на месяц", "");
-	time = prompt('Введите дату в формате YYY-MM-DD', ""),
-	answay1 = prompt('Введите обязательную статью расходов в этом месяце', ""),
-	answay2 = prompt('Во сколько обойдется?', "");
+	time = prompt('Введите дату в формате YYY-MM-DD', "");
+
+let answay1 = prompt('Введите обязательную статью расходов в этом месяце', ""),
+	answay2 = prompt('Во сколько обойдется?', ""),
+	answay3 = prompt('Введите обязательную статью расходов в этом месяце', ""),
+	answay4 = prompt('Во сколько обойдется?', "");
 
 let appData = {
 	bud: money,
 	timeData: time,
-	expenses: { answay1: answay2 },
+	expenses: {},
 	optionalExpenses: "",
 	income: "",
 	savings: false
 };
 
-alert('Бюджет на 1 день = ' + (money/30));
+appData.expenses[answay1] = answay2;
+appData.expenses[answay3] = answay4;
 
+
+
+alert('Бюджет на 1 день = ' + (money/30));
 
